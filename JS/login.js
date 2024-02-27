4f48f40918d25501263bcc2aed3d5b9c6d6b63ce
IgorAzzi <igor.salati@hotmail.com>
2024-02-27 17:03:49 -0300

    Criação do projeto

---
+++ JS/login.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('.login-input');
+const button = document.querySelector('login-button');
+
+function validateInput() {
+
+}
+
+const validateInput = (event) => {}
+
+input.addEventListener('input', validateInput);
